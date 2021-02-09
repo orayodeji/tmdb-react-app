f148009ce69fb560473604f2a410a313774a98ac
orayodeji <orayodeji@gmail.com>
2021-02-09 11:39:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import { useState, useEffect } from 'react'
+import Movies from './components/Movies';
+import Addmovie from './components/Addmovie';
+
+
 
 function App() {
+  const [movies, setMovies] = useState([])
+  const imgpath= "https://image.tmdb.org/t/p/w500"
+
+  useEffect(()=>{
+    const getMovies = async()=>{
+      const res = await fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b001c57ee146a9efef888fb472fa0d23&page=1");
+      const data = await res.json()
+      return setMovies(data.results)
+    }
+    getMovies()
+  }, [])
+  
+  
+
+  const findMovie = async (title)=>{
+    const res = await fetch (`https://api.themoviedb.org/3/search/movie?&api_key=b001c57ee146a9efef888fb472fa0d23&query=${title}`)
+    const data = await res.json();
+    return setMovies(data.results)  
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Addmovie findMovie={findMovie}/>
+      {movies.length > 0 ? <Movies movies={movies} imgpath={imgpath}/> : 'No Movies to Show'}
+      
     </div>
   );
 }
